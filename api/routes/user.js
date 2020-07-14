5b3b09c5d072a3ab8bdb9019f34e09342a54cca7
LaluPrasad M <laluprasad.73515274@gmail.com>
2020-07-14 23:18:32 +0530

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,9 +4,9 @@ const multer = require('multer');
 
 const UserController = require("../controllers/user");
 
-router.get('/login',UserController.user_get_login);
+router.get('/',UserController.user_get_login);
 
-router.post('/login',UserController.user_post_login);
+router.post('/',UserController.user_post_login);
 
 
 module.exports = router;
